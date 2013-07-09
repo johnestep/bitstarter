e6b3d5eccbcbcd8d4cb0579c27829e15a13f37a0
John Estep <jestep@jettsoft.com>
2013-07-09 23:18:05 +0000

    Grader.js work

--- grader.js
+++ grader.js
@@ -20,9 +20,10 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
-
+var sys = require('util');
 var fs = require('fs');
 var program = require('commander');
+var restler = require('restler');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
@@ -39,6 +40,10 @@ var assertFileExists = function(infile) {
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
+var cheerioURLFile = function(URLFile) {
+    return cheerio.load(URLFile);
+};
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
@@ -54,6 +59,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     }
     return out;
 };
+var checkURLFile = function(urlfile, checksfile) {
+    $ = cheerioURLFile(urlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -62,13 +78,31 @@ var clone = function(fn) {
 };
 
 if(require.main == module) {
+   var outJson;
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+ 
+    if (program.url){
+    restler.get(program.url).on('complete', function(result) {
+      if (result instanceof Error) {
+        sys.puts('Error: ' + result.message);
+        this.retry(5000); // try again after 5 sec
+      } else {
+       // sys.puts(result);
+      var checkJson = checkURLFile(result, program.checks);
+      outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+     }
+   })
+  } else {
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+  }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
